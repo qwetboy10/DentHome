11114ae7dae3e98a81351c6cb5bf8d143f228d5e
Tristan Wiesepape <wiesepapetristan@gmail.com>
2019-11-02 16:55:55 -0500

    dent home version 1

---
+++ index.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+import os
+
+env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('index.html')
+uptime = os.popen('uptime').read()
+ip = os.popen('dig @resolver1.opendns.com ANY myip.opendns.com +short').read()
+s = ["NetworkManager"]
+services = []
+for a in s:
+    services.append(os.popen('systemctl status ' + a + ' | grep Active').read())
+print(template.render(uptime=uptime, ip=ip, services=services))
